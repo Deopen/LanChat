4a8dd99714c8e45d868806acb2c905416b180a0a
Omid Yaghoubi <deopenmail@gmail.com>
2013-03-09 19:40:57 +0330

    Create ImpConsts.java

---
+++ ImpConsts.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lanchat;
+
+/**
+ *
+ * @author deopenmobile
+ */
+public interface ImpConsts {
+    
+    static final int chat_port_number=12525;     
+}
